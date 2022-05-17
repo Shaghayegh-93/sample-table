7b4d4e3773796bf0cdb19c07e2e50bd1d30b5ede
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-05-17 14:58:02 +0430

    render Table component

--- components/Main.js
+++ components/Main.js
@@ -2,11 +2,12 @@ import React from "react";
 import Navbar from "./Navbar";
 import Table from "./Table";
 
-const Main = () => {
+const Main = ({ todo, user }) => {
+  // console.log(user);
   return (
-    <div className="bg-white m-auto mt-7 rounded-xl">
+    <div className="bg-white m-auto mt-7 rounded-xl  mx-auto">
       <Navbar />
-      <Table />
+      <Table todo={todo} user={user} />
     </div>
   );
 };
