4cb2a1c1c005a0c868d763fdd97958b979b99c05
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-05-19 13:26:56 +0430

    add search state and search function

--- components/Main.js
+++ components/Main.js
@@ -5,17 +5,15 @@ import TablePagination from "./TablePagination";
 import { useState, useEffect } from "react";
 
 const Main = ({ todo, user }) => {
-  console.log("todo", todo);
   const [todo_state, set_todo_state] = useState([]);
-
   const [currentPage, setCurrentPage] = useState(1);
-  // console.log("current-page", currentPage);
+  const [search, setSearch] = useState("");
+  const [sortProducts, setSortProducts] = useState([]);
   const count_page = 10;
   const all_page = todo.length / count_page;
-  // console.log("alll-page: ", all_page);
-  useEffect(() => {
-    // console.log("prev-page", prev_page);
+  console.log("todo", todo);
 
+  useEffect(() => {
     const next_page = currentPage * count_page;
     const prev_page = (currentPage - 1) * count_page;
     let data = todo.slice(prev_page, next_page);
@@ -32,11 +30,33 @@ const Main = ({ todo, user }) => {
     currentPage < all_page && setCurrentPage(currentPage + 1);
   };
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    // setSearch("");
+  };
+  const filterdTodo = todo.filter((td) =>
+    td.title.toLowerCase().includes(search)
+  );
+  console.log("search", search);
+  console.log(todo.filter((t) => t.title.toLowerCase().includes("qu")));
+  const searchHandler = (e) => {
+    if (e.key === "Enter") {
+      //   setSearch(filterdTodo);
+      // setSearch("");
+      // }
+    }
+  };
 
   return (
     <div className="bg-white m-auto mt-7 rounded-xl  mx-auto">
-      <Navbar />
-      <Table todo={todo_state} user={user} />
+      <Navbar
+        search={search}
+        changeHandler={changeHandler}
+        searchHandler={searchHandler}
+        filterdTodo={filterdTodo}
+      />
+      <Table todo={todo_state} user={user} filterdTodo={filterdTodo} />
       <TablePagination
         todo={todo_state}
         user={user}
