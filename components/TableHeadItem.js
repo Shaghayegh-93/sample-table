7ee746d8b4b12da2b582237c03b7a66c3fa2609e
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-05-21 15:41:14 +0430

    give sortClickHandler as props and change logic of onclick for select tag

--- components/TableHeadItem.js
+++ components/TableHeadItem.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-const TableHeadItem = ({ data, sortHandler, sort }) => {
+const TableHeadItem = ({ data, sortHandler, sort, sortClickHandler }) => {
   console.log("sort", sort);
   return data.map((item) => {
     return item.icon ? (
       <th
         scope="col"
-        className="p-4 py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+        className="  p-4 py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
         key={item.path}
       >
         <>
-          <select onChange={sortHandler}>
-            <option onClick={() => sortClickHandler("asc")} value="asc">
-              asc
-            </option>
+          <select onChange={() => sortHandler()}>
+            <option value="asc">asc</option>
             <option value="desc">desc</option>
           </select>
         </>
@@ -23,7 +21,9 @@ const TableHeadItem = ({ data, sortHandler, sort }) => {
     ) : (
       <th
         scope="col"
-        className="p-4 py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+        className={`${item.name === "Contact" ? "hidden md:block" : ""} ${
+          item.name === "Action" ? "rounded-tr-2xl" : ""
+        }  p-4 py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase  dark:text-gray-400`}
         key={item.path}
       >
         {item.name}
