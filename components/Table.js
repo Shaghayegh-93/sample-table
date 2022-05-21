9fb01721c61fed1aa85a3e8d6c465295bbfeab9a
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-05-21 15:39:34 +0430

    add the function and state for sorting todos

--- components/Table.js
+++ components/Table.js
@@ -5,51 +5,49 @@ import Search from "./../public/svg/search-icon.svg";
 // import ArrowUp from "./../public/svg/icon";
 // import ArrowDown from "./../public/svg/icon(1)";
 import TableHeadItem from "./../components/TableHeadItem";
-const Table = ({ user, todo, filterdTodo }) => {
+const Table = ({ user, todo, filterdTodo, todo_state }) => {
   // console.log("todo_state", todo_state);
   console.log("todo", todo);
 
   const theadData = [
     { path: "user", name: "User" },
     { path: "Contact", name: "Contact" },
-    { path: "To-Do", name: "To-Do", icon: <Search /> },
+    { path: "To-Do", name: "To-Do" },
     { path: "Completed", name: "Completed" },
     { path: "Action", name: "Action" },
   ];
-  const [sort, setSort] = useState("");
-  // const [filterdData, setFilteredData] = useState([]);
-  const [filterdTodo, setFilterdTodo] = useState([]);
-  let filterdSortData = [...filterdTodo];
-  // console.log("filterdData", filterdData);
-  const sortHandler = (e) => {
-    setSort(e.target.value);
-  };
-  // const [sortData, setSortData] = useState("");
-  // const [order, setOrder] = useState("asc");
-  useEffect(() => {
-    if (sort === "asc") {
-      // setFilteredData((data) =>
-      //   [...data].sort((a, b) => a.title.toLoweCase() - b.title.toLoweCase())
-      // );
-      const soretedData = filterdSortData.sort(
-        (a, b) => b.title.toLoweCase() - a.title.toLoweCase()
-      );
-      setFilterdTodo(soretedData);
-    }
-  }, [sort]);
-  // const [sortPath, setSortPath] = useState();
-  // const [sortOrder, setSortOrder] = useState();
-  // console.log("sort", sort);
 
+  const [sort_data, set_sort_data] = useState("asc");
+
+  const [data, set_data] = useState([]);
+  console.log("data", data);
+
+  let filterdSortData = [...filterdTodo];
+
+  const compareFn = (a, b) => {
+    if (a.title > b.title) return +1;
+    else if (a.title < b.title) return -1;
+    else return 0;
+  };
+
+  const sortHandler = (e) => {
+    const type = e.target.value;
+    set_sort_data(type);
+    const soretedData = filterdSortData.sort((a, b) => compareFn(a, b));
+    console.log("sort_daya: ", soretedData);
+    set_data(soretedData);
+  };
+
+  console.log("data: ", data);
   return (
-    <div className="flex flex-col bg-purple-900">
-      <div className="overflow-x-auto shadow-md sm:rounded-lg">
-        <div className="inline-block min-w-full align-middle">
-          <div className=" bg-red-200 overflow-hidden">
-            <table className="  p-4 min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-              <thead className="bg-gray-100 ">
-                <tr className="">
-                  <th scope="col" className="p-4">
+    <div className="flex flex-col  rounded-2xl	 pb-6">
+      <div className="overflow-x-auto sm:rounded-lg rounded-2xl border border-[#E0E7ED]">
+        <div className="inline-block min-w-full align-middle ">
+          <div className="  overflow-hidden ">
+            <table className="   p-4 min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+              <thead className="bg-gray-100 	 ">
+                <tr className="		">
+                  <th scope="col" className="p-4 rounded-tl-2xl	">
                     <div className="flex items-center">
                       <input
                         type="checkbox"
@@ -60,12 +58,12 @@ const Table = ({ user, todo, filterdTodo }) => {
                   <TableHeadItem
                     data={theadData}
                     sortHandler={sortHandler}
-                    sort={sort}
+                    sort={sort_data}
                   />
                 </tr>
               </thead>
-              <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                {filterdTodo?.map((item) => (
+              <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700 	">
+                {todo_state?.map((item) => (
                   <TableRow key={item} className="" todo={item} user={user} />
                 ))}
               </tbody>
